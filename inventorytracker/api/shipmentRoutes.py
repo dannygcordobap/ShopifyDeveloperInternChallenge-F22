77b0cfa706eee77bbc50dc2c10c7c56700743581
dannygcordobap <dannygcordobap@gmail.com>
2022-05-11 14:32:11 -0400

    Comment shipmentRoutes.py

--- inventorytracker/api/shipmentRoutes.py
+++ inventorytracker/api/shipmentRoutes.py
@@ -1,5 +1,4 @@
 import json
-from traceback import print_exc
 from flask import request
 from inventorytracker import server
 from inventorytracker.api import shipmentController
@@ -9,6 +8,10 @@ SHIPMENT_ENDPOINT = "/api/v1/shipment"
 @server.route(f"{SHIPMENT_ENDPOINT}/<id>", methods = ["GET", "PUT", "DELETE"])
 @server.route(f"{SHIPMENT_ENDPOINT}", methods = ["GET", "POST"])
 def shipment(id = None):
+    """
+    Handles the retrieval of all shipments, shipment creation/deletion, and the 
+    editing of a shipment's recipient
+    """
     try:
         if id:
             if request.method == "GET":
@@ -42,7 +45,6 @@ def shipment(id = None):
         return json.loads(response), status
     
     except:
-        print_exc()
         return json.loads({
             "error": "Server error in shipmentRoutes.shipment"
         }), 500
@@ -50,6 +52,10 @@ def shipment(id = None):
 @server.route(f"{SHIPMENT_ENDPOINT}/<id>/items/<itemID>", methods = ["DELETE"])
 @server.route(f"{SHIPMENT_ENDPOINT}/<id>/items", methods = ["POST", "PUT"])
 def shipmentItems(id, itemID = None):
+    """
+    Handles the addition and removal of items from a shipment as well as
+    editing shipment item quantities.
+    """
     try:
         data = request.get_json()
         if request.method == "POST" and data:
@@ -64,7 +70,6 @@ def shipmentItems(id, itemID = None):
             }), 400
         return json.loads(response), status 
     except:
-        print_exc()
         return json.loads({
             "error": "Server error in shipmentRoutes.shipmentItems"
         }), 500
